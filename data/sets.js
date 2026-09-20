36472a62fe814ad383984fb337e84e51d8c3d33b
sapfi24 <vadmal24@gmail.com>
2026-09-21 09:06:01 +1000

    Add files via upload

---
+++ data/sets.js
@@ -0,0 +1,116 @@
+const sets = [
+  {
+    slug: "pink",
+    name: "Pink Inventory",
+    tag: "POPULAR",
+    price: "2 000 ₽",
+    duration: "6 дней",
+
+    description:
+      "Цельный розовый инвентарь в едином стиле. Подходит для тех, кто хочет получить готовый красивый сет без покупки всех предметов за полную стоимость.",
+
+    value: "≈ 150 000 ₽",
+
+    items: [
+      "Предмет 01",
+      "Предмет 02",
+      "Предмет 03",
+      "Предмет 04",
+      "Предмет 05",
+      "Предмет 06"
+    ],
+
+    images: [
+      "/sets/pink/1.jpg",
+      "/sets/pink/2.jpg",
+      "/sets/pink/3.jpg",
+      "/sets/pink/4.jpg"
+    ]
+  },
+
+  {
+    slug: "purple",
+    name: "Purple Inventory",
+    tag: "NEW",
+    price: "2 500 ₽",
+    duration: "7 дней",
+
+    description:
+      "Фиолетовый сет с собранным визуальным стилем и подбором предметов в одной цветовой гамме.",
+
+    value: "≈ 180 000 ₽",
+
+    items: [
+      "Предмет 01",
+      "Предмет 02",
+      "Предмет 03",
+      "Предмет 04",
+      "Предмет 05"
+    ],
+
+    images: [
+      "/sets/purple/1.jpg",
+      "/sets/purple/2.jpg",
+      "/sets/purple/3.jpg",
+      "/sets/purple/4.jpg"
+    ]
+  },
+
+  {
+    slug: "red",
+    name: "Red Inventory",
+    tag: "LIMITED",
+    price: "3 000 ₽",
+    duration: "7 дней",
+
+    description:
+      "Контрастный красный сет с яркими предметами и агрессивной цветовой подачей.",
+
+    value: "≈ 220 000 ₽",
+
+    items: [
+      "Предмет 01",
+      "Предмет 02",
+      "Предмет 03",
+      "Предмет 04",
+      "Предмет 05"
+    ],
+
+    images: [
+      "/sets/red/1.jpg",
+      "/sets/red/2.jpg",
+      "/sets/red/3.jpg",
+      "/sets/red/4.jpg"
+    ]
+  },
+
+  {
+    slug: "ice",
+    name: "Ice Inventory",
+    tag: "POPULAR",
+    price: "2 700 ₽",
+    duration: "7 дней",
+
+    description:
+      "Холодная цветовая палитра и спокойный визуальный стиль. Готовый сет для цельного инвентаря.",
+
+    value: "≈ 200 000 ₽",
+
+    items: [
+      "Предмет 01",
+      "Предмет 02",
+      "Предмет 03",
+      "Предмет 04",
+      "Предмет 05"
+    ],
+
+    images: [
+      "/sets/ice/1.jpg",
+      "/sets/ice/2.jpg",
+      "/sets/ice/3.jpg",
+      "/sets/ice/4.jpg"
+    ]
+  }
+];
+
+export default sets;
