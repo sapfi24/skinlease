e98d8f65a1656bf84d6d1acd6aedc178c0bf31cb
sapfi24 <vadmal24@gmail.com>
2026-09-21 09:36:27 +1000

    Update page.js

--- app/sets/[slug]/page.js
+++ app/sets/[slug]/page.js
@@ -1,7 +1,36 @@
-export default function SetPage() {
+import sets from "../../../data/sets";
+
+export default async function SetPage({ params }) {
+  const { slug } = await params;
+
+  const set = sets.find(item => item.slug === slug);
+
+  if (!set) {
+    return (
+      <div style={{ color: "white", padding: "100px" }}>
+        Сет не найден: {slug}
+      </div>
+    );
+  }
+
   return (
-    <div style={{ color: "red", padding: "100px", fontSize: "40px" }}>
-      TEST 123
+    <div style={{ color: "white", padding: "100px" }}>
+      <h1>{set.name}</h1>
+
+      <p>{set.description}</p>
+
+      <p>{set.price}</p>
+
+      <p>{set.duration}</p>
+
+      <img
+        src={set.images[0]}
+        alt={set.name}
+        style={{
+          width: "500px",
+          borderRadius: "20px"
+        }}
+      />
     </div>
   );
 }
