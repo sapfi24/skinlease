17a3888f35df52d6be608a5f62dfdd8b2409e678
sapfi24 <vadmal24@gmail.com>
2026-09-21 08:34:13 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
 import Hero from "../components/Hero";
 import Sets from "../components/Sets";
+import Advantages from "../components/Advantages";
+import HowItWorks from "../components/HowItWorks";
+import CTA from "../components/CTA";
 
 export default function Home() {
   return (
     <>
+      <div className="site-background" />
+
       <Hero />
       <Sets />
+      <Advantages />
+      <HowItWorks />
+      <CTA />
     </>
   );
 }
