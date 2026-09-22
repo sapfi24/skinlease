5d892ef40a951509c0c408e6846215c20d201a33
Sapfi <vadmal24@gmail.com>
2026-09-23 01:35:36 +1000

    стрелка тг тест бага

--- components/Header.js
+++ components/Header.js
@@ -38,24 +38,7 @@ export default function Header() {
             rel="noreferrer"
             className="nav-button"
           >
-            Telegram
-            Telegram 
-<svg
-  className="arrow-icon"
-  width="14"
-  height="14"
-  viewBox="0 0 14 14"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <path
-    d="M3 11L11 3M5 3H11V9"
-    stroke="currentColor"
-    strokeWidth="1.8"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  />
-</svg>
+            ТЕСТ123
           </a>
 
         </nav>
