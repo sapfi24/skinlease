f1dbcbfe042408620c2f9c647d217c4c0baef949
Sapfi <vadmal24@gmail.com>
2026-09-23 02:19:13 +1000

    финальная мобильная стрелка

--- components/CTA.js
+++ components/CTA.js
@@ -29,13 +29,33 @@ export default function CTA() {
             </p>
 
             <a
-              href="https://t.me/USERNAME"
+              href="https://t.me/sapfiofcl"
               target="_blank"
               rel="noreferrer"
               className="button-primary"
             >
               Написать в Telegram
-              <span>&#8599;</span>
+
+<span className="cta-arrow-desktop">
+</span>
+
+<svg
+  className="cta-arrow-mobile"
+  width="18"
+  height="18"
+  viewBox="0 0 14 14"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+>
+  <path
+    d="M3 11L11 3M5 3H11V9"
+    stroke="currentColor"
+    strokeWidth="1.6"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>
             </a>
 
           </div>
